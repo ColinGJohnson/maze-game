9c8f50241e8e22d8cfcf20f1dc741db11310d676
Colin Johnson <colin@cgj.dev>
2025-05-24 17:43:09 -0700

    Switch to Vite for build

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import MazeGame from "./modules/MazeGame.js";
 
 // create and start a new maze game when the window loads
 window.onload = function init() {
-    const mazeGame = new MazeGame();
+    new MazeGame();
 }
