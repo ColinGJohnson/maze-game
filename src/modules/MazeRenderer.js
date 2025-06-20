af088336d0ce06c3d2a6aa0703ae8c9114ed2098
Colin Johnson <colin@cgj.dev>
2025-06-19 23:08:15 -0700

    Clean up render logic

--- src/modules/MazeRenderer.js
+++ src/modules/MazeRenderer.js
@@ -18,15 +18,32 @@ export default class MazeRenderer {
   }
 
   /**
-   * Resizes the canvas to match the current window size.
+   * Draws a maze on the canvas associated with this MazeRenderer.
    *
    * @param {MazeGame} mazeGame the MazeGame to draw.
    */
-  updateDimensions(mazeGame) {
+  render(mazeGame) {
+    this.updateCanvasDimensions(mazeGame.maze.size);
+    this.drawBackground();
+
+    if (mazeGame.gameState !== GameState.START) {
+      this.drawMaze(mazeGame);
+      this.drawPlayer(mazeGame.player);
+    }
+
+    this.text.innerHTML = this.getTopText(mazeGame);
+  }
+
+  /**
+   * Resizes the canvas to fit the current window size.
+   *
+   * @param {number} mazeSize - mazeSize the MazeGame to draw. Will evenly divide the resulting canvas width.
+   */
+  updateCanvasDimensions(mazeSize) {
     const container = document.querySelector(".maze-container");
 
-    this.cellWidthPx = Math.round(container.clientWidth / mazeGame.maze.size);
-    const size = this.cellWidthPx * mazeGame.maze.size;
+    this.cellWidthPx = Math.round(container.clientWidth / mazeSize);
+    const size = this.cellWidthPx * mazeSize;
     this.canvas.width = size;
     this.canvas.height = size;
 
@@ -35,43 +52,25 @@ export default class MazeRenderer {
   }
 
   /**
-   * Draws a maze on the canvas associated with this MazeRenderer.
-   *
-   * @param {MazeGame} mazeGame the MazeGame to draw.
+   * Fill the canvas with a plain black background.
    */
-  render(mazeGame) {
-    // update canvas dimensions in case the window was resized
-    this.updateDimensions(mazeGame);
-
-    // draw black background
+  drawBackground() {
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 
-    // if game has been started at least once
-    if (mazeGame.gameState !== GameState.START) {
-      // fill maze squares
-      this.drawMaze(mazeGame);
-
-      // draw player square
-      const player = mazeGame.player;
-      this.ctx.fillStyle = "grey";
-      let topLeft = {
-        x: Math.round(player.x * this.cellWidthPx),
-        y: Math.round(player.y * this.cellWidthPx),
-      };
-      this.ctx.fillRect(topLeft.x, topLeft.y, this.cellWidthPx, this.cellWidthPx);
-    }
-
-    if (mazeGame.gameState === GameState.START) {
-      this.text.innerHTML = "Press the space bar to start <br> Use WASD or arrow keys to move";
-    } else if (mazeGame.gameState === GameState.IN_GAME) {
-      const currentTimeMs = new Date().getTime();
-      const secondsPassed = (currentTimeMs - mazeGame.gameStartTimestamp) / 1000;
-      this.text.innerHTML = `<b>${secondsPassed.toFixed(2) + "s"}</b>`;
-    } else if (mazeGame.gameState === GameState.END) {
-      const secondsPassed = (mazeGame.gameEndTimestamp - mazeGame.gameStartTimestamp) / 1000;
-      this.text.innerHTML = `Finished in <b>${secondsPassed.toFixed(2)}s</b><br>Press any key to restart`;
-    }
+  /**
+   * Draws the player on the canvas within the maze game.
+   *
+   * @param {Player} player - The maze game object containing the player's information.
+   */
+  drawPlayer(player) {
+    this.ctx.fillStyle = "grey";
+    const topLeft = {
+      x: Math.round(player.x * this.cellWidthPx),
+      y: Math.round(player.y * this.cellWidthPx),
+    };
+    this.ctx.fillRect(topLeft.x, topLeft.y, this.cellWidthPx, this.cellWidthPx);
   }
 
   /**
@@ -112,5 +111,27 @@ export default class MazeRenderer {
       hue = hue < 0 ? 0 : hue;
       return "hsl(" + hue + ", 90%, 50%)";
     }
+  }
+
+  /**
+   * @param {MazeGame} mazeGame - The maze game object containing game state information.
+   * @param {string} text - The default text to display if no game state text applies.
+   * @returns {string} The text to display based on the current game state:
+   *   - At start: Shows instructions to start and movement controls
+   *   - During game: Shows elapsed time in seconds
+   *   - At end: Shows completion time and restart instructions
+   */
+  getTopText(mazeGame, text) {
+    if (mazeGame.gameState === GameState.START) {
+      text = "Press the space bar to start <br> Use WASD or arrow keys to move";
+    } else if (mazeGame.gameState === GameState.IN_GAME) {
+      const currentTimeMs = new Date().getTime();
+      const secondsPassed = (currentTimeMs - mazeGame.gameStartTimestamp) / 1000;
+      text = `<b>${secondsPassed.toFixed(2) + "s"}</b>`;
+    } else if (mazeGame.gameState === GameState.END) {
+      const secondsPassed = (mazeGame.gameEndTimestamp - mazeGame.gameStartTimestamp) / 1000;
+      text = `Finished in <b>${secondsPassed.toFixed(2)}s</b><br>Press any key to restart`;
+    }
+    return text;
   }
 }
